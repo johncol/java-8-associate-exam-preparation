6b3b0c224f079a36b6ef5cb15cb16d213b801142
John Cely <john.cely@prodigious.com>
2016-03-07 20:58:17 -0500

    Examples

---
+++ src/tests/ArrayLists.java
@@ -0,0 +1,17 @@
+package tests;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayLists {
+
+    public static void main(String[] args) {
+        new ArrayLists();
+    }
+
+    {
+        List<String> list = new ArrayList<>();
+
+    }
+
+}
