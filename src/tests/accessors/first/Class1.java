f3a9d55c464050c7db45b654e7b74bd1e6b7f74f
John Cely <John Cely>
2016-03-13 06:57:54 -0500

    Examples

---
+++ src/tests/accessors/first/Class1.java
@@ -0,0 +1,9 @@
+package tests.accessors.first;
+
+public class Class1 {
+
+    protected void hi() {
+        System.out.println("HI");
+    }
+
+}
---
+++ src/tests/accessors/second/Class2.java
@@ -0,0 +1,12 @@
+package tests.accessors.second;
+
+import tests.accessors.first.Class1;
+
+public class Class2 extends Class1 {
+
+    {
+        Class2 class2 = new Class2();
+        class2.hi();
+    }
+
+}
