9743cd7dacb5a4b7b64543cbbfff707461e4402d
John Cely <John Cely>
2016-03-19 19:38:49 -0500

    Examples

---
+++ src/tests/abstracttests/Test.java
@@ -0,0 +1,13 @@
+package tests.abstracttests;
+
+public abstract class Test {
+
+    protected abstract void test();
+
+}
+
+abstract class Test2 extends Test {
+
+    protected abstract void test();
+
+}
